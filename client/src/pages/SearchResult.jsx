b125c44580dd6ce1e45904160309943179b0a6ac
sumanth-dps <dondamuripavansatyasumanth@gmail.com>
2025-02-13 20:07:16 +0530

    Update SearchResult.jsx

--- client/src/pages/SearchResult.jsx
+++ client/src/pages/SearchResult.jsx
@@ -17,7 +17,7 @@ const SearchResult = () => {
             <div className='flex items-center gap-3 text-2xl font-bold text-violet-500 border-b pb-3 mb-5'>
                 <h4 >Search Result For: {q}  </h4>
             </div>
-            <div className='grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10'>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10'>
                 {blogData && blogData.blog.length > 0
                     ?
                     blogData.blog.map(blog => <BlogCard key={blog._id} props={blog} />)
