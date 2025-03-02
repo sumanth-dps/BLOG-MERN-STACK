143ee3f4e746ad3f8c91be45ee9ef685c2bc7280
sumanth-dps <dondamuripavansatyasumanth@gmail.com>
2025-03-02 20:16:39 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="text-sm text-center bg-gray-50 py-4">
+    <div className="text-sm text-center bg-background/50 py-4">
       © Copyright 2025 | Designed & Developed By:{" "}
       <a
         href="https://www.linkedin.com/in/sumanth-dondamuri"
