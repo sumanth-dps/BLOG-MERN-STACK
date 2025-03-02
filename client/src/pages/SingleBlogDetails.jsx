12824e6915200e7b938218b96b2fe679623fb734
sumanth-dps <dondamuripavansatyasumanth@gmail.com>
2025-03-02 21:28:04 +0530

    Update SingleBlogDetails.jsx

--- client/src/pages/SingleBlogDetails.jsx
+++ client/src/pages/SingleBlogDetails.jsx
@@ -35,7 +35,7 @@ const SingleBlogDetails = () => {
                                     <AvatarImage src={data.blog.author.avatar} />
                                 </Avatar>
                                 <div>
-                                <div classname="flex items-center">
+                                <div classname="flex flex-row gap-2 items-center">
                                     <p className='font-bold'>{data.blog.author.name}</p>
                                     {data.blog.author.role === "admin" && (
                       <img
